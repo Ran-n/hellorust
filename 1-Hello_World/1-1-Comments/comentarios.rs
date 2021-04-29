548eebcc34fce4b4ebf95159aad8f5d209fe955d
Ran-n <ran-n@tutanota.com>
2021-04-29 23:19:53 +0200

    cambios

---
+++ 1-Hello_World/1-1-Comments/comentarios.rs
@@ -0,0 +1,32 @@
+/*
+#!
+#+ Autor:	Ran#
+#+ Creado:	29/04/2021 23:15:55
+#+ Editado:	29/04/2021 23:17:25
+*/
+
+// liña simple
+/*
+ * multiliña
+ *
+ *
+ *
+ */
+
+/*
+ 
+tamén así funcan
+
+*/
+
+/*
+/// facer cousas
+//! facer cousas 2
+*/
+
+fn main(){
+    // podense poñer os comentarios dentro da mesma liña de expresión
+    //let x = 5 + /* 90 + */ 5;
+    let x = 5 + 90 + 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
